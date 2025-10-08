4c2753d03af5786751ce75899dccb6328c73cc16
Ba6aka <bam6ykba6aka@gmail.com>
2025-10-08 13:49:44 +0300

    fix: set correct content-type for CSS files in serveStatic function

--- server/handlers/serve-static.js
+++ server/handlers/serve-static.js
@@ -4,6 +4,10 @@ const { handle404 } = require('./handle404.js')
 function serveStatic(request, response) {
   const path = "public/" + (request.url.slice(1) || "index.html")
 
+  if (request.url.endsWith(".css")) {
+    response.setHeaders({ 'content-type': 'text/css' })
+  }
+
   createReadStream(path).on('error', handle404(response)).pipe(response)
 }
 
