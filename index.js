7715673abcf5c567be6c0115c209974937f3463f
Ba6aka <bam6ykba6aka@gmail.com>
2025-10-08 13:35:47 +0300

    fix: use environment variable for server port in MongoDB connection

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb')
 const { runServer } = require('./server/run-server.js')
 const { handleRequest } = require('./server/handlers/handle-request.js')
 
-const { DB_USER, DB_PWD } = process.env
+const { DB_USER, DB_PWD, PORT } = process.env
 let db, usersCollection, allUsers
 const connectionString = `mongodb+srv://${DB_USER}:${DB_PWD}@cluster0.zkbmsbi.mongodb.net/`
 const client = new MongoClient(connectionString)
@@ -12,5 +12,5 @@ const dbName = 'registartionAPP'
 
 client.connect().then(console.log('Connected to MongoDB Atlas')).then(async () => {
   db = client.db(dbName)
-  runServer(999, handleRequest(db))
+  runServer(PORT || 999, handleRequest(db))
 })
