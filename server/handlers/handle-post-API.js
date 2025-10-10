94d4d5f461e1583beaa0f9e6c15b8a1650510953
Ba6aka <bam6ykba6aka@gmail.com>
2025-10-10 19:42:59 +0300

    refactor: simplify handlePostAPI by using a handlers object for routing

--- server/handlers/handle-post-API.js
+++ server/handlers/handle-post-API.js
@@ -3,13 +3,16 @@ const { logInEndPoint } = require('./post-end-points/log-in-end-point.js')
 const { authEndPoint } = require('./post-end-points/auth-end-point.js')
 const { logOutEndPoint } = require('./post-end-points/log-out-end-point.js')
 
+const handlers = {
+  'user': userEndPoint,
+  'log-in': logInEndPoint,
+  'auth': authEndPoint,
+  'log-out': logOutEndPoint
+}
+
 async function handlePostAPI(request, response, route, db) {
   usersCollection = db.collection('users')
-
-  if (route == 'user') userEndPoint(request, response, usersCollection)
-  else if (route == 'log-in') logInEndPoint(request, response, usersCollection)
-  else if (route == 'auth') authEndPoint(request, response, usersCollection)
-  else if (route == "log-out") logOutEndPoint(request, response, usersCollection)
+  handlers[route]?.(request, response, usersCollection)
 }
 
 module.exports = { handlePostAPI }
